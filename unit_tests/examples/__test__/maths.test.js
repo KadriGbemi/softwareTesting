7def92dd2a051cf97dbdb4545804309427ac67cb
“KadriGbemi” <kadri.oluwagbemi@gmail.com>
2021-07-14 10:32:58 +0300

    End to end testing

--- unit_tests/examples/__test__/maths.test.js
+++ unit_tests/examples/__test__/maths.test.js
@@ -65,33 +65,120 @@ test("Check for numeric ranges", async () => {
   // expect(50).not.toBeDivisibleByExternalValue(); //Falsy Matcher
 });
 
-// expect.extend({
-//   async toBeDivisibleByExternalValue(received) {
-//     const projects = await getProjectsWithAsyncAwait();
-//     const externalValue = projects.length;
+// expect.extend(matchers) - Async Matchers
+// https://jestjs.io/docs/expect#async-matchers
+expect.extend({
+  async toBeDivisibleByExternalValue(received) {
+    const projects = await getProjectsWithAsyncAwait();
+    const externalValue = projects.length;
 
-//     const pass = received % externalValue == 0;
-//     if (pass) {
-//       return {
-//         message: () =>
-//           `expected ${received} not to be divisible by ${externalValue}`,
-//         pass: true,
-//       };
-//     } else {
-//       return {
-//         message: () =>
-//           `expected ${received} to be divisible by ${externalValue}`,
-//         pass: false,
-//       };
-//     }
-//   },
-// });
+    const pass = received % externalValue == 0;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be divisible by ${externalValue}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be divisible by ${externalValue}`,
+        pass: false,
+      };
+    }
+  },
+});
 
-// test("is divisible by external value", () => {
-//   expect(100).toBeDivisibleByExternalValue();
-//   expect(101).not.toBeDivisibleByExternalValue();
-// });
-// test("is divisible by external value", async () => {
-//   await expect(4).toBeDivisibleByExternalValue();
-//   await expect(5).not.toBeDivisibleByExternalValue();
-// });
+test("is divisible by external value", () => {
+  expect(100).toBeDivisibleByExternalValue();
+  expect(5).not.toBeDivisibleByExternalValue();
+});
+test("is divisible by external value", async () => {
+  await expect(4).toBeDivisibleByExternalValue();
+  await expect(5).not.toBeDivisibleByExternalValue();
+});
+
+// Custom matchers https://jestjs.io/docs/expect#custom-matchers-api
+const { diff } = require("jest-diff");
+expect.extend({
+  toBe(received, expected) {
+    const options = {
+      comment: "Check for equality with Object.is",
+      isNot: this.isNot,
+      promise: this.promise,
+    };
+    const pass = Object.is(received, expected);
+    const message = pass
+      ? () =>
+          this.utils.matcherHint("toBe", undefined, undefined, options) +
+          "\n\n" +
+          `Expected: not ${this.utils.printExpected(expected)}\n` +
+          `Received: ${this.utils.printReceived(received)}`
+      : () => {
+          const diffString = diff(expected, received, {
+            expand: this.expand,
+          });
+          return (
+            this.utils.matcherHint("toBe", undefined, undefined, options) +
+            "\n\n" +
+            (diffString && diffString.includes("- Expect") //Shows full differences and errors
+              ? `Difference:\n\n${diffString}`
+              : `Expected: ${this.utils.printExpected(expected)}\n` +
+                `Received: ${this.utils.printReceived(received)}`)
+          );
+        };
+
+    return { actual: received, message, pass };
+  },
+});
+
+test("Check for equality", async () => {
+  expect("banana").toBe("banana");
+  expect("banana").not.toBe("rain");
+});
+
+// Custom snapshot matchers https://jestjs.io/docs/expect#custom-snapshot-matchers
+
+const { toMatchSnapshot } = require("jest-snapshot");
+
+expect.extend({
+  toMatchTrimmedSnapshot(received, length) {
+    return toMatchSnapshot.call(
+      this,
+      received.substring(1, length),
+      "toMatchTrimmedSnapshot"
+    );
+  },
+});
+
+it("stores not less than the 10 characters", () => {
+  expect("A very long sentences testing").toMatchTrimmedSnapshot(10);
+});
+
+it("stores not less than the 15 characters", () => {
+  expect("extra long string oh my gerd").toMatchTrimmedSnapshot(15);
+});
+
+async function getTempateByID(templateID) {
+  const projects = await getProjectsWithAsyncAwait();
+  const getProjectByID = projects.filter(
+    (project) => project.id === templateID
+  );
+  return getProjectByID;
+}
+
+expect.extend({
+  async toMatchAPITemplate(received) {
+    // console.log("received", received)
+    return toMatchSnapshot.call(
+      this,
+      received.toString(),
+      "toMatchAPITemplate"
+    );
+  },
+});
+
+it("Compare By UI template", async () => {
+  const getTemplate = await getTempateByID(1);
+  await expect(getTemplate[0].template).toMatchAPITemplate();
+});
--- unit_tests/examples/api/requests.js
+++ unit_tests/examples/api/requests.js
@@ -12,13 +12,9 @@ const getProjectsNoAsyncAwait = () => {
 // Async-await
 async function getProjectsWithAsyncAwait() {
   const response = await fetch("http://localhost:3000/projects");
-
-  const data = await response.json();
-  
-  console.log(data);
-  return data
+  return response.json();
 }
-getProjectsWithAsyncAwait()
+getProjectsWithAsyncAwait();
 // const getProjectsWithAsyncAwait = async () => {
 //     const response = await fetch("http://localhost:3000/projects");
 //     return response.json()
